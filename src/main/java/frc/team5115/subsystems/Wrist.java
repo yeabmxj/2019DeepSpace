19bac522e160253cc4dcf1354c6b34a3fad2911e
James Pham <james20902@gmail.com>
2019-02-07 23:23:53 -0500

    wrist

--- src/main/java/frc/team5115/subsystems/Wrist.java
+++ src/main/java/frc/team5115/subsystems/Wrist.java
@@ -6,20 +6,18 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Wrist {
-
-    ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("Neutral",
-            "Up",
-            "Down",
-            "Left",
-            "Right"));
+public class Wrist extends Subsystem{
 
     TalonSRX xController;
     TalonSRX yController;
 
-    String state = "Neutral";
-
     public Wrist(){
+        dictionary = new ArrayList<>(Arrays.asList("Neutral",
+                "Up",
+                "Down",
+                "Left",
+                "Right"));
+
         xController = new TalonSRX(6);
         yController = new TalonSRX(7);
     }
@@ -30,19 +28,6 @@ public class Wrist {
 
     public void moveY(double speed){
         yController.set(ControlMode.PercentOutput, speed);
-    }
-
-    public int setState(String state){
-        if(dictionary.contains(state)){
-            this.state = state;
-            return 0;
-        } else {
-            return -1;
-        }
-    }
-
-    public String currentState(){
-        return this.state;
     }
 
     public void update(){

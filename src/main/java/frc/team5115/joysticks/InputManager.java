ff8ac69e4ec22e72b81fda8d5c982dcb23ee25d6
James Pham <james20902@gmail.com>
2019-02-08 23:12:27 -0500

    misc.

--- src/main/java/frc/team5115/joysticks/InputManager.java
+++ src/main/java/frc/team5115/joysticks/InputManager.java
@@ -18,22 +18,24 @@ public class InputManager {
     JSONObject controllerData;
 
     public InputManager() {
+        getJSON();
+        checkControllers();
+    }
+
+    public void getJSON(){
         try {
-            controllerData = new JSONObject(new JSONParser().parse(new FileReader("Controllers.json")));
-            primary = new Controller(0, controllerData.getJSONObject(new Joystick(0).getName()));
-            if (new Joystick(1).getButtonCount() > 0) {
-                secondary = new Controller(1, controllerData.getJSONObject(new Joystick(0).getName()));
-            } else {
-                secondary = primary;
-            }
-        } catch (Exception e){
+            controllerData = new JSONObject(new JSONParser().parse(new FileReader("/home/lvuser/Controllers.json")));
+            System.out.println(controllerData);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void checkControllers(){
+        if (controllerData == null){
+            getJSON();
+        }
         try {
-            controllerData = new JSONObject(new JSONParser().parse(new FileReader("Controllers.json")));
             primary = new Controller(0, controllerData.getJSONObject(new Joystick(0).getName()));
             if (new Joystick(1).getButtonCount() > 0) {
                 secondary = new Controller(1, controllerData.getJSONObject(new Joystick(0).getName()));
